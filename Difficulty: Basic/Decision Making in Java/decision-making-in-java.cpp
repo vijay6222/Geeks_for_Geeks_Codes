06d648db7a49109870cbdb0c9651edac22828f70
vijay6222 <158019883+vijay6222@users.noreply.github.com>
2025-10-31 10:40:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Decision Making in Java/decision-making-in-java.cpp
@@ -0,0 +1,13 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    string compareNM(int n, int m) {
+        // code here
+        if(n<m) return "lesser";
+        else if (n==m) return "equal";
+        else return "greater";
+
+
+    }
+};
