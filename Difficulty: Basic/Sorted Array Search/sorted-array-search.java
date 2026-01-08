a132b813a8909362bfbd0eff190c4ccf893bb2ce
vijay6222 <158019883+vijay6222@users.noreply.github.com>
2026-01-08 20:48:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sorted Array Search/sorted-array-search.java
@@ -0,0 +1,13 @@
+class Solution {
+    static boolean searchInSorted(int arr[], int k) {
+         for(int i=0;i<arr.length;i++){
+             if(arr[i]==k)
+             {
+                 return true;
+             }
+         }
+         return false;
+        
+    }
+}
+
